0122c99492c4a6198b19dc4fde1297023510e58e
nekacurry <anneka498@gmail.com>
2021-05-13 20:37:28 -0500

    Update __init__.py

--- game_app/__init__.py
+++ game_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app.config import Config
+from game_app.config import Config
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
